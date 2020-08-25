0b732be5bc9d941b2faa706881f04ee9074683df
hp pc <royalrahul646@gmail.com>
2020-08-25 21:45:01 +0530

    fourth commit

---
+++ myproj4/src/myproj4/ProjFour.java
@@ -0,0 +1,12 @@
+package myproj4;
+
+public class ProjFour {
+
+	public static void main(String[] args) {
+		System.out.println("hadd hai yar");
+		System.out.println("mai bolu");
+		System.out.println("law of attraction");
+
+	}
+
+}
